6814a1219b12d0193872735892b602d4f7469b51
Sander Mangel <sander@sandermangel.nl>
2017-04-09 14:14:36 -0700

    mark client integration tests as skipped when httpbin host is not available

--- tests/Integration/Api/ClientTest.php
+++ tests/Integration/Api/ClientTest.php
@@ -16,15 +16,19 @@ class ClientTest extends TestCase
      */
     public function testExecuteGetCall()
     {
-        return;
-        $client = new Client(new ConfigMock(), 'GET', '/get', [
-            'testkey' => 'testvariable',
-            'testassockey' => [
-                'testmultikey' => 'testmultivalue'
-            ]
-        ], []);
+        $data = [];
+        try {
+            $client = new Client(new ConfigMock(), 'GET', '/get', [
+                'testkey' => 'testvariable',
+                'testassockey' => [
+                    'testmultikey' => 'testmultivalue'
+                ]
+            ], []);
 
-        $data = $client->executeCall();
+            $data = $client->executeCall();
+        } catch (ApiException $error) {
+            $this->markTestSkipped($error->getMessage());
+        }
 
         $this->assertEquals('200', $data['httpcode']);
         $this->assertEquals('testmultivalue', $data['body']->args->{'testassockey[testmultikey]'});
@@ -37,15 +41,19 @@ class ClientTest extends TestCase
      */
     public function testExecutePostCall()
     {
-        return;
-        $client = new Client(new ConfigMock(), 'POST', '/post', [
-            'testkey' => 'testvariable',
-            'testassockey' => [
-                'testmultikey' => 'testmultivalue'
-            ]
-        ], []);
+        $data = [];
+        try {
+            $client = new Client(new ConfigMock(), 'POST', '/post', [
+                'testkey' => 'testvariable',
+                'testassockey' => [
+                    'testmultikey' => 'testmultivalue'
+                ]
+            ], []);
 
-        $data = $client->executeCall();
+            $data = $client->executeCall();
+        } catch (ApiException $error) {
+            $this->markTestSkipped($error->getMessage());
+        }
 
         $this->assertEquals('200', $data['httpcode']);
         $this->assertEquals('testkey=testvariable&testassockey%5Btestmultikey%5D=testmultivalue', $data['body']->data);
@@ -57,12 +65,16 @@ class ClientTest extends TestCase
      */
     public function testExecuteDeleteCall()
     {
-        return;
-        $client = new Client(new ConfigMock(), 'DELETE', '/delete', [
-            'sku' => 'test-sku'
-        ], []);
+        $data = [];
+        try {
+            $client = new Client(new ConfigMock(), 'DELETE', '/delete', [
+                'sku' => 'test-sku'
+            ], []);
 
-        $data = $client->executeCall();
+            $data = $client->executeCall();
+        } catch (ApiException $error) {
+            $this->markTestSkipped($error->getMessage());
+        }
 
         $this->assertEquals('200', $data['httpcode']);
         $this->assertEquals('sku=test-sku', $data['body']->data);
@@ -74,12 +86,16 @@ class ClientTest extends TestCase
      */
     public function testExecutePutCall()
     {
-        return;
-        $client = new Client(new ConfigMock(), 'PUT', '/put', [
-            'sku' => 'test-sku'
-        ], []);
+        $data = [];
+        try {
+            $client = new Client(new ConfigMock(), 'PUT', '/put', [
+                'sku' => 'test-sku'
+            ], []);
 
-        $data = $client->executeCall();
+            $data = $client->executeCall();
+        } catch (ApiException $error) {
+            $this->markTestSkipped($error->getMessage());
+        }
 
         $this->assertEquals('200', $data['httpcode']);
         $this->assertEquals('sku=test-sku', $data['body']->data);
